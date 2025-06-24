aad429d17e98dbbf8d6a2a6804d73445061fd1d0
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-06-24 08:47:37 +0800

    add torch as ref

--- 01-vector-addition/benchmark.py
+++ 01-vector-addition/benchmark.py
@@ -4,7 +4,6 @@ from Triton.native import solve as triton_add
 from Tilelang.native import solve as tile_add
 
 
-
 def benchmark(f, n=None, nvtx=None, *args, **kwargs):
     # trigger jit
     f(*args, **kwargs)
@@ -32,20 +31,45 @@ def benchmark(f, n=None, nvtx=None, *args, **kwargs):
     return start.elapsed_time(end) / 1000
 
 
-size = 1024 * 1024 * 30
+sizes = [128 * 2 ** p for p in range(0, 20)]
+sizes = [127, 129] + sizes
+y1 = []
+y2 = []
+y3 = []
+
+for size in sizes:
+    a = torch.randn(size, device='cuda')
+    b = torch.randn(size, device='cuda')
+    c = torch.zeros(size, device='cuda')
+
+    triton_add(a, b, c, size)
+
+    assert torch.allclose(c, a + b)
+
+    y1.append(benchmark(lambda: triton_add(a, b, c, size), 100, nvtx='triton'))
+    y2.append(benchmark(lambda: tile_add(a, b, c, size), 100, nvtx='tilelang'))
+    y3.append(benchmark(lambda: torch.add(a, b, out=c), 100, nvtx='torch'))
 
 
-a = torch.randn(size, device='cuda')
-b = torch.randn(size, device='cuda')
-c = torch.zeros(size, device='cuda')
-c2 = torch.zeros_like(c)
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df1 = pd.DataFrame({'x': sizes, 'y': y1})
+df2 = pd.DataFrame({'x': sizes, 'y': y2})
+df3 = pd.DataFrame({'x': sizes, 'y': y3})
 
-triton_add(a, b, c, size)
-tile_add(a, b, c2, size)
+plt.figure(figsize=(10, 6))
+ax = plt.gca()
 
-assert torch.allclose(c, c2)
+sns.regplot(x='x', y='y', data=df1, ax=ax, label='Triton', scatter_kws={'s': 50})
+sns.regplot(x='x', y='y', data=df2, ax=ax, label='Tilelang', scatter_kws={'s': 50})
+sns.regplot(x='x', y='y', data=df3, ax=ax, label='Torch', scatter_kws={'s': 50})
 
+plt.title('Vectorized add')
+plt.xlabel('Size')
+plt.ylabel('Time')
 
-print(f'triton: {benchmark(lambda: triton_add(a, b, c, size), 10, nvtx='triton')}')
-print(f'tilelang: {benchmark(lambda: tile_add(a, b, c, size), 10, nvtx='tilelang')}')
+plt.legend()
+
+plt.savefig('01-result.png')
