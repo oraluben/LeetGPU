c287bbe2152e054f37689bf0bcd7c2ada3360d41
BobHuang <30999153+sBobHuang@users.noreply.github.com>
2025-10-09 07:57:15 +0800

    add log_sum_exp softmax (#8)

--- CuTeDSL/13-softmax.py
+++ CuTeDSL/13-softmax.py
@@ -1,54 +1,49 @@
 import cutlass.cute as cute
 from cutlass.utils import SmemAllocator
-import cutlass
-from typing import Callable, Optional
-import math
-import operator
-
-@cute.jit
-def warp_reduce(
-    val: cute.Numeric,
-    op: Callable,
-    width: cutlass.Constexpr[int] = cute.arch.WARP_SIZE,
-) -> cute.Numeric:
-    for i in cutlass.range_constexpr(int(math.log2(width))):
-        val = op(val, cute.arch.shuffle_sync_bfly(val, offset=1 << i))
-    return val
-
-@cute.jit
-def block_reduce(
-    val: cute.Numeric, op: Callable, reduction_buffer: cute.Tensor, init_val: cute.Numeric = 0.0
-) -> cute.Numeric:
-    lane_idx, warp_idx = cute.arch.lane_idx(), cute.arch.warp_idx()
-    col_idx = warp_idx
-    if lane_idx == 0:
-        reduction_buffer[col_idx] = val
-    cute.arch.barrier()
-    block_reduce_val = reduction_buffer[lane_idx]
-    return warp_reduce(block_reduce_val, op)
-
 
 @cute.kernel
-def softmax_kernel(input: cute.Tensor, output: cute.Tensor, s_layout: cute.Layout):
+def softmax_log_sum_exp_kernel(input: cute.Tensor, output: cute.Tensor, s_layout: cute.Layout):
     tidx, _, _ = cute.arch.thread_idx()
     bdimx, _, _ = cute.arch.block_dim()
 
-    smem = SmemAllocator()
-
-    shared = smem.allocate_tensor(input.element_type, s_layout, 16)
-
-    local_max = input[tidx]
-    for i in range(tidx + bdimx, input.shape[0], bdimx):
-        local_max = max(local_max, input[i])
-
-    max_val = block_reduce(local_max, cute.arch.fmax, shared, init_val=float('-inf'))
-
+    local_max = float('-inf')
     local_sum = 0.0
     for i in range(tidx, input.shape[0], bdimx):
-        local_sum += cute.exp(input[i] - max_val)
+        x = input[i]
+        new_max = max(local_max, x)
+        new_sum = 0.0
+        if local_max == float('-inf'):
+            new_sum = cute.exp(x - new_max)
+        else:
+            new_sum = cute.exp(local_max - new_max) * local_sum + cute.exp(x - new_max)
+        local_max = new_max
+        local_sum = new_sum
 
-    local_sum = warp_reduce(local_sum, operator.add)
-    sum_exp = block_reduce(local_sum, operator.add, shared)
+    smem = SmemAllocator()
+    smax = smem.allocate_tensor(input.element_type, s_layout, 16)
+    ssum = smem.allocate_tensor(input.element_type, s_layout, 16)
+    smax[tidx] = local_max
+    ssum[tidx] = local_sum
+    cute.arch.barrier()
+
+    stride = bdimx // 2
+    while stride > 0:
+        if tidx < stride:
+            a_max, b_max = smax[tidx], smax[tidx + stride]
+            a_sum, b_sum = ssum[tidx], ssum[tidx + stride]
+            new_max, new_sum = max(a_max, b_max), 0.0
+            if a_max == float('-inf'):
+                new_sum = b_sum
+            elif b_max == float('-inf'):
+                new_sum = a_sum
+            else:
+                new_sum = cute.exp(a_max - new_max) * a_sum + cute.exp(b_max - new_max) * b_sum;
+            smax[tidx], ssum[tidx] = new_max, new_sum
+        cute.arch.barrier()
+        stride = stride // 2
+
+    max_val, sum_exp = smax[0], ssum[0]
+    cute.arch.barrier()
 
     for i in range(tidx, input.shape[0], bdimx):
         output[i] = cute.exp(input[i] - max_val) / sum_exp
@@ -59,4 +54,4 @@ def solve(input: cute.Tensor, output: cute.Tensor, N: cute.Int32):
     block_dim = 1024, 1, 1
     grid_dim = 1, 1, 1
     s_layout = cute.make_layout((1024), stride=(1))
-    softmax_kernel(input, output, s_layout).launch(grid=grid_dim, block=block_dim)
+    softmax_log_sum_exp_kernel(input, output, s_layout).launch(grid=grid_dim, block=block_dim)
