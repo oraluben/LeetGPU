d0b97294aba3267da00dcf89fd7e44130494cb36
Yichen Yan <wenji.yyc@alibaba-inc.com>
2026-01-21 17:44:59 +0800

    update add example

--- 01-vector-addition/Tilelang/native.py
+++ 01-vector-addition/Tilelang/native.py
@@ -5,24 +5,21 @@ import torch
 
 
 @tilelang.jit
-def add(M, block_M, dtype="float32"):
-    @T.prim_func
-    def add_kernel(
-        A: T.Tensor((M, ), dtype),
-        B: T.Tensor((M, ), dtype),
-        C: T.Tensor((M, ), dtype),
-    ):
-        num_per_thread = 8
-        with T.Kernel(T.ceildiv(M, block_M * num_per_thread), threads=128) as bx:
-            for local_x, i in T.Parallel(block_M, num_per_thread):
-                x = (bx * block_M + local_x) * num_per_thread
-                C[x + i] = A[x + i] + B[x + i]
+def add(a, b, c, M, block_M, dtype="float32"):
+    a: T.Tensor((M,), dtype)
+    b: T.Tensor((M,), dtype)
+    c: T.Tensor((M,), dtype)
 
-    return add_kernel
+    num_per_thread = 8
+
+    with T.Kernel(T.ceildiv(M, block_M * num_per_thread), threads=128) as bx:
+        for local_x, i in T.Parallel(block_M, num_per_thread):
+            x = (bx * block_M + local_x) * num_per_thread
+            c[x + i] = a[x + i] + b[x + i]
+            pass
 
 
 def solve(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, N: int):
     BLOCK_SIZE = 128
-    jit_kernel = add(N, BLOCK_SIZE)
 
-    jit_kernel(a, b, c)
+    add(a, b, c, N, BLOCK_SIZE)
--- 01-vector-addition/benchmark.py
+++ 01-vector-addition/benchmark.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 
 from Triton.native import solve as triton_add
@@ -35,8 +36,9 @@ def benchmark(f, n=None, nvtx=None, *args, **kwargs):
     return start.elapsed_time(end)
 
 
-sizes = [128 * 2**p for p in range(0, 10)]
+sizes = [128 * 2**p for p in range(0, 20)]
 sizes = [127, 129] + sizes
+
 
 tests = {
     "Triton": triton_add,
@@ -50,66 +52,104 @@ if find_spec("cutlass") is None:
     pass
 elif version("nvidia-cutlass-dsl") >= "4.3.4":
     import cutlass.cute as cute
-    from CuTeDSL.native import solve as cute_kernel
 
-    def cutedsl_add(
-        a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, n: int, cache={}
-    ):
-        if n not in cache:
-            a_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
-            b_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
-            c_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
+    def cutedsl_add(cutedsl_kernel):
+        def _inner(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, n: int, cache={}):
+            if n not in cache:
+                a_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
+                b_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
+                c_fake = cute.runtime.make_fake_compact_tensor(cute.Float32, (n,))
 
-            cache[n] = cute.compile(
-                cute_kernel, a_fake, b_fake, c_fake, n, options="--enable-tvm-ffi"
-            )
+                cache[n] = cute.compile(
+                    cutedsl_kernel,
+                    a_fake,
+                    b_fake,
+                    c_fake,
+                    n,
+                    options="--enable-tvm-ffi",
+                )
 
-        cache[n](a, b, c, n)
+            cache[n](a, b, c, n)
 
-    tests["CuTeDSL"] = cutedsl_add
+        return _inner
+
+    from CuTeDSL.native import solve as solve_native
+    from CuTeDSL.peak_performance import solve as solve_peak
+    from CuTeDSL.use_tv_layout import solve as solve_tv
+
+    tests["CuTeDSL"] = cutedsl_add(solve_native)
+    tests["CuTeDSL Opt"] = cutedsl_add(solve_peak)
+    # tests["CuTeDSL TV"] = cutedsl_add(solve_tv)
 else:
     tests["Tilelang (CuTeDSL)"] = lambda a, b, c, size: tile_cutedsl_add(a, b, c, size)
 
 
-results = {}
+def main(n=100, check_result=False):
+    results = {}
+
+    for i, size in enumerate(sizes):
+        a = torch.randn(size, device="cuda")
+        b = torch.randn(size, device="cuda")
+        c = torch.zeros(size, device="cuda")
+
+        for title, test in tests.items():
+            if check_result:
+                test(a, b, c, size)
+                assert torch.allclose(c, a + b), f'{c}, {a + b}'
+
+            results.setdefault(title, []).append(
+                benchmark(lambda: test(a, b, c, size), n)
+            )
+
+    return results
 
 
-for i, size in enumerate(sizes):
-    a = torch.randn(size, device="cuda")
-    b = torch.randn(size, device="cuda")
-    c = torch.zeros(size, device="cuda")
-
-    triton_add(a, b, c, size)
-
-    assert torch.allclose(c, a + b)
-
-    for title, test in tests.items():
-        results.setdefault(title, []).append(
-            benchmark(lambda: test(a, b, c, size), 100)
-        )
-
-
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 6))
-ax = plt.gca()
-
-for title, result in results.items():
-    sns.regplot(
-        x="x",
-        y="y",
-        data=pd.DataFrame({"x": sizes, "y": result}),
-        ax=ax,
-        label=title,
-        scatter_kws={"s": 50},
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("--no-plot", dest="plot", action="store_false", default=True)
+    p.add_argument(
+        "-k", "--kernel", type=str, default=None, nargs="+", choices=tests.keys()
     )
+    p.add_argument("--shapes", dest="shapes", type=int, default=None, nargs="+")
+    p.add_argument("-i", "--iters", type=int, default=100)
+    p.add_argument("-d", "--dry-run", dest="dry", action="store_true", default=False)
+    args = p.parse_args()
 
-plt.title("Vectorized add")
-plt.xlabel("Size")
-plt.ylabel("Time (ms)")
+    if args.kernel:
+        tests = {k: v for k, v in tests.items() if k in args.kernel}
 
-plt.legend()
+    if args.shapes:
+        sizes = [sizes[i] for i in args.shapes]
 
-plt.savefig("01-result.png")
+    if args.dry:
+        print(args)
+        print(sizes)
+        exit()
+
+    results = main(args.iters)
+
+    if args.plot:
+        import seaborn as sns
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+        plt.figure(figsize=(10, 6))
+        ax = plt.gca()
+
+        for title, result in results.items():
+            sns.regplot(
+                x="x",
+                y="y",
+                data=pd.DataFrame({"x": sizes, "y": result}),
+                ax=ax,
+                label=title,
+                scatter_kws={"s": 50},
+            )
+
+        plt.title("Vectorized add")
+        plt.xlabel("Size")
+        plt.ylabel("Time (ms)")
+
+        plt.legend()
+
+        plt.savefig("01-result.png")
